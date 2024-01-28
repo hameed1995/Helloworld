4dde3c3125128458cff1767452e87d1ef898e867
hameed1995 <103354959+hameed1995@users.noreply.github.com>
2024-01-28 18:35:18 +0530

    Add files via upload

---
+++ SampleProgram.java
@@ -0,0 +1,14 @@
+package com.helloworld;
+
+public class SampleProgram {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		
+		int number=10;
+		int number1=15;
+		int sum=number+number1;
+		System.out.println(sum);
+	}
+
+}
